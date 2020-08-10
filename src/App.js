31fc8b76f28918b9d46d30651b2ff8d8711fbc75
Chue Yang <chueyangib@gmail.com>
2020-08-10 15:56:03 -0500

    map employees updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Employee from "./components/Employee";
 import Search from "./components/Search";
 import Navbar from "./components/Navbar";
 import employees from "./employees.json";
 
 
-function App() {
-  return (
-  <div className="container">
-    <Navbar />
-    <Search />
-    <Employee employees ={employees} />
-  </div>
-  );
+class App extends Component {
+
+  state={
+    employees
+  };
+  
+  render() {
+    return (
+    <div className="container">
+      <Navbar />
+      <Search />
+      <table className="table">
+          <thead className="thead-light">
+              <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Name</th>
+              <th scope="col">Title</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+              </tr>
+          </thead>
+          <tbody>
+            {this.state.employees.map(employee => (
+              <Employee 
+                id={employee.id}
+                key={employee.id}
+                name={employee.name}
+                title={employee.title}
+                email={employee.email}
+                phone={employee.phone}
+              />
+            ))}
+        </tbody>
+      </table>
+    </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,31 +2,13 @@ import React from "react";
 
 function Employee(props){
  return (
-<div>
-    <table className="table">
-        <thead className="thead-light">
-            <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Name</th>
-            <th scope="col">Title</th>
-            <th scope="col">Email</th>
-            <th scope="col">Phone</th>
-            </tr>
-        </thead>
-        <tbody>
-    {/* Mapping each employee by row */}
-        {props.employees.map(item=> 
-            <tr>
-            <th scope="row">{item.id}</th>
-            <td>{item.name}</td>
-            <td>{item.title}</td>
-            <td>{item.email}</td>
-            <td>{item.phone}</td>
-            </tr>
-        )}
-        </tbody>
-    </table>
-</div>
+     <tr>
+        <td>{props.id}</td>
+        <td>{props.name}</td>
+        <td>{props.title}</td>
+        <td>{props.email}</td>
+        <td>{props.phone}</td>
+    </tr>
  );
 }
 
