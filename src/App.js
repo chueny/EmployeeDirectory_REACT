c99fbf62a173671d7cd5d90aec5a465eff733532
Chue Yang <chueyangib@gmail.com>
2020-08-10 14:57:10 -0500

    mapped each employee

--- src/App.js
+++ src/App.js
@@ -10,23 +10,7 @@ function App() {
   <div className="container">
     <Navbar />
     <Search />
-    <Employee 
-      name={employees[0].name}
-      title={employees[0].title}
-      email={employees[0].email}
-    />
-     <Employee 
-      name={employees[10].name}
-      title={employees[10].title}
-      email={employees[10].email}
-    />
-    
-    <Employee 
-      name={employees[11].name}
-      title={employees[11].title}
-      email={employees[11].email}
-    />
-
+    <Employee employees ={employees} />
   </div>
   );
 }
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -3,10 +3,30 @@ import React from "react";
 function Employee(props){
  return (
 <div>
- <p>Employee!</p>
- <p>Name:</p> {props.name}
- <p>Email:</p> {props.email}
- </div>
+    <table className="table">
+        <thead className="thead-light">
+            <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Title</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            </tr>
+        </thead>
+        <tbody>
+    {/* Mapping each employee by row */}
+        {props.employees.map(item=> 
+            <tr>
+            <th scope="row">{item.id}</th>
+            <td>{item.name}</td>
+            <td>{item.title}</td>
+            <td>{item.email}</td>
+            <td>{item.phone}</td>
+            </tr>
+        )}
+        </tbody>
+    </table>
+</div>
  );
 }
 
