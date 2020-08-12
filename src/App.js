e7a6eeea1656aaede5dc297a8d690454ee913fac
Chue Yang <chueyangib@gmail.com>
2020-08-11 22:07:45 -0500

    nothing

--- src/App.js
+++ src/App.js
@@ -1,47 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Employee from "./components/Employee";
-import Search from "./components/Search";
+// import Search from "./components/Search";
 import Navbar from "./components/Navbar";
 import employees from "./employees.json";
 
 
-class App extends Component {
-
-  state={
-    employees
-  };
-  
-  render() {
-    return (
+function App () {
+  return (
     <div className="container">
       <Navbar />
-      <Search />
-      <table className="table">
-          <thead className="thead-light">
-              <tr>
-              <th scope="col">Id</th>
-              <th scope="col">Name</th>
-              <th scope="col">Title</th>
-              <th scope="col">Email</th>
-              <th scope="col">Phone</th>
-              </tr>
-          </thead>
-          <tbody>
-            {this.state.employees.map(employee => (
-              <Employee 
-                id={employee.id}
-                key={employee.id}
-                name={employee.name}
-                title={employee.title}
-                email={employee.email}
-                phone={employee.phone}
-              />
-            ))}
-        </tbody>
-      </table>
+      {/* <Search />   WE MAY NEED TO REMOVE THIS COMPONENT*/}
+      <Employee employees={employees} />
     </div>
-    )
-  }
+  )
 }
 
 export default App;
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,15 +1,55 @@
 import React from "react";
+// import employees from "../employees.json";
 
 function Employee(props){
- return (
-     <tr>
-        <td>{props.id}</td>
-        <td>{props.name}</td>
-        <td>{props.title}</td>
-        <td>{props.email}</td>
-        <td>{props.phone}</td>
-    </tr>
- );
+
+// state={
+//     employees
+// };
+
+return (
+    <div>
+        <form>
+        <input
+           // value={this.state.password}
+            name="password"
+            //onChange={this.handleInputChange}
+            type="password"
+            placeholder="Password"
+          />
+          {" "}
+          <button type="submit" class="btn btn-primary" >Search</button>
+          {/* <button onClick={this.handleFormSubmit}>Submit</button> */}
+        </form>
+        {"  "}
+
+
+        {/* Table header for all the employees */}
+        <table className="table">
+            <thead className="thead-light">
+                <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Title</th>
+                <th scope="col">Email</th>
+                <th scope="col">Phone</th>
+                </tr>
+            </thead>
+            <tbody>
+        {/* Mapping each employee by row */}
+            {props.employees.map(item=> 
+                <tr>
+                <th scope="row">{item.id}</th>
+                <td>{item.name}</td>
+                <td>{item.title}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+                </tr>
+            )}
+            </tbody>
+        </table>
+    </div>
+);
 }
 
 export default Employee;
