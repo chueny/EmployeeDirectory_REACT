2a204e681fed6ea653e48d09f3e3951fa5ca4d6f
Chue Yang <chueyangib@gmail.com>
2020-08-13 10:35:13 -0500

    build folder

---
+++ build/precache-manifest.2e52ea0a9891f57f121a792dbf65b73e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497d706c563458e4fec1694da8626cac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc6c614a6ade9b89d7c2",
+    "url": "/static/css/main.3e371bc7.chunk.css"
+  },
+  {
+    "revision": "7e5f53e8bae7018c2c95",
+    "url": "/static/js/2.2b6a84b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b6a84b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc6c614a6ade9b89d7c2",
+    "url": "/static/js/main.20186b28.chunk.js"
+  },
+  {
+    "revision": "ed0fae93a1705cfe61bf",
+    "url": "/static/js/runtime-main.61c3aabd.js"
+  }
+]);
